a778e6a7ac36322f638a5139c46e6307aa995e34
Matt Marschall <mmarschall@redmars.com.au>
2022-01-10 15:34:19 +1100

    Commence Card hitboxes and interaction

--- core/src/au/com/redmars/Card.java
+++ core/src/au/com/redmars/Card.java
@@ -1,14 +1,18 @@
 package au.com.redmars;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Card {
     Integer faceValue;
     Integer suit;
     Sprite image;
+    boolean canGrab;
+    Rectangle hitbox; 
     Card(Integer faceValue, Integer suit,Sprite image) {
         this.faceValue = faceValue;
         this.suit = suit;
         this.image = image;
+        this.canGrab = false;
     }
 }
