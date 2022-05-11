9d4a92d500c8f23e2b6bbda99ba8dd6c0ba96772
Matt Marschall <mmarschall@redmars.com.au>
2022-05-11 14:38:27 +1000

    Added 128pt font

--- core/src/au/com/redmars/Solitaire.java
+++ core/src/au/com/redmars/Solitaire.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class Solitaire extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
+	public BitmapFont largeFont;
 	public ShapeRenderer shapeRenderer;
 
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont(Gdx.files.internal("font.fnt"));
+		largeFont = new BitmapFont(Gdx.files.internal("Ageny-128pt.fnt"));
 		batch.enableBlending();
 		shapeRenderer = new ShapeRenderer();
 		this.setScreen(new MainMenuScreen(this));
